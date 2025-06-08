7a8a53efb14358bfa16eca9f48487f3607ab4c0e
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-06-08 21:02:39 +0530

    run the initial server and used graphql to fetch only titl e

--- server/index.js
+++ server/index.js
@@ -2,12 +2,29 @@ const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { expressMiddleware } = require("@apollo/server/exp")
+const { expressMiddleware } = require("@apollo/server/express4")
 
 
 async function startServer() {
     const app = express();
-    const server = new ApolloServer();
+    const server = new ApolloServer({
+        typeDefs: `
+        type Todo{
+        id: ID!
+        title : String!
+        completed : Boolean
+        }
+
+        type Query{
+        getTodos : [Todo]
+        }
+        `,
+        resolvers: {
+            Query: {
+                getTodos: () => [{ id: 1, title: "Jai shree ram", completed: false }]
+            }
+        }
+    });
 
     app.use(bodyParser.json());
     app.use(cors());
@@ -18,3 +35,5 @@ async function startServer() {
 
     app.listen(8000, () => console.log("sever started at port 8000 "))
 }
+
+startServer();
