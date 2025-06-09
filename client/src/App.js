da40fa4d0dc04c7adeb0cb07b60dddb4ca24317c
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-06-09 14:37:28 +0530

    fetched the data from graphql server on the ui sceen

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { gql, useQuery } from '@apollo/client';
 
@@ -21,22 +20,11 @@ query ExampleQuery {
 
 function App() {
   const { data, loading } = useQuery(query)
+  if (loading)
+    return <h1>Loading...</h1>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JSON.stringify(data)}
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
 
 const client = new ApolloClient({
-  uri: "https://localhost:8000/graphql",
+  uri: "http://localhost:8000/graphql",
   cache: new InMemoryCache()
 });
 
